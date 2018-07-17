9a1990ce18c312c2127bbd17ac76f708e66b22e5
Jeffrey Jiang <anon0503@gmail.com>
2018-07-17 05:54:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = "YOUR-NAME is the champion"
+var element = document.createElement('h1');
+const newHeader = null;
+document.newHeader appendChild(element);
