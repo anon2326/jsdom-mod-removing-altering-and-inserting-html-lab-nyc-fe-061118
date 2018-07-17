a0797f96d6d5432f287a478e0dae9adb33565047
Jeffrey Jiang <anon0503@gmail.com>
2018-07-17 05:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove();
 /* Create your new element here and assign it to newHeader */
+document.body.appendChild(h1);
 let newHeader = null;
-newHeader.setAttribute("id", "victory")
-newHeader = document.createTextNode("(your name) is the champion!")
-document.body.appendChild('newHeader')
+newHeader.setAttribute("id", "victory");
+newHeader = document.createTextNode("(your name) is the champion!");
+document.body.appendChild('newHeader');
