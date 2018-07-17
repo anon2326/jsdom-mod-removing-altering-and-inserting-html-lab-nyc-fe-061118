02e59eab783033e107ef8c7d1431e87eff1493c8
Jeffrey Jiang <anon0503@gmail.com>
2018-07-17 05:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 /* Enter the code to remove the main node element under this comment */
 main.remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
-newHeader.
-h1#victory.textContext = "YOUR-NAME is the champion";
+const newHeader = "YOUR-NAME is the champion"
