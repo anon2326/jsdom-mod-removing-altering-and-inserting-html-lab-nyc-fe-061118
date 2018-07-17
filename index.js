937235ad5f9e993bbec9a7eca0fe6bfef943e594
Jeffrey Jiang <anon0503@gmail.com>
2018-07-17 05:09:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ main.remove();
 /* Create your new element here and assign it to newHeader */
 document.body.appendChild(element);
 var newHeader = document.createElement('h1');
+var textnode = document.createTextNode("(your name) is the champion!");
+newHeader.appendChild(textnode);
+newHeader.setAttribute("id", "victory");
