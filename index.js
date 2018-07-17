c25b630656e7f7316175cc424e3e1effb92947ac
Jeffrey Jiang <anon0503@gmail.com>
2018-07-17 05:00:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 main.remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+var headerwun = document.createElement('H1');
+'H1'.setAttribute("id", "victory")
+var headercon = document.createTextNode("(your name) is the champion!")
+document.body.appendChild('h1')
